25e1672a23294d490fab70ec1cff4a1fe070eeeb
Chris Were <chris@mozzler.com.au>
2021-01-23 20:23:05 +1030

    Display databases once connected. Support loading data from different apps.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,15 +1,28 @@
 <template>
   <div class="hello">
-        <p class="btn" verida-vault-login-btn="true">
+      <div class="" v-if="!loggedIn">
+        <input type="text" v-model="appName" />
+        <p class="btn" verida-vault-login-btn="true" @click="startLogin">
           <span>{{ buttonText }}</span>
           <VeridaLogo :width="67" textColor="white"/>
         </p>
+      </div>
+      <div v-if="loggedIn" id="result-container">
+        <p>Welcome <strong>{{ veridaApp.user.did }}</strong>, you are now connected to <strong>{{ veridaApp.appName }}</strong>!</p>
+        <div class="databases">
+          <p>The following databases were found in this application:</p>
+          <ol>
+            <li v-for="(item, index) in databases" :key="index">{{ item.dbName }}</li>
+          </ol>
+        </div>
+      </div>
   </div>
 </template>
 
 <script>
 import { veridaVaultLogin } from '@verida/vault-auth-client'
 import VeridaLogo from './VeridaLogo'
+import Verida from '@verida/datastore'
 
 export default {
   name: 'HelloWorld',
@@ -19,12 +32,39 @@ export default {
   components: {
     VeridaLogo
   },
-  mounted() {
-    veridaVaultLogin({
-      loginUri: 'http://192.168.1.29:8080/mobile/auth-request.html',
-      serverUri: 'ws://localhost:7001'
-    });
+  data() {
+    return {
+      appName: 'Verida: Auth client demo',
+      loggedIn: false,
+      databases: [],
+      veridaApp: null
+    }
   },
+  methods: {
+    startLogin () {
+      console.log("starting login with ", this.appName)
+      veridaVaultLogin({
+        loginUri: 'http://192.168.1.29:8080/mobile/auth-request.html',
+        serverUri: 'ws://192.168.1.29:7001',
+        appName: this.appName,
+        callback: async (response) => {
+          console.log(`${response.did} succesfully logged in`)
+
+          const veridaApp = new Verida({
+              did: response.did,
+              signature: response.signature,
+              appName: this.appName
+          })
+
+          await veridaApp.connect(true)
+          this.loggedIn = true
+          this.veridaApp = window.veridaApp = veridaApp
+          let ds = await window.veridaApp.openDatastore("https://schemas.verida.io/storage/database/schema.json")
+          this.databases = await ds.getMany()
+        }
+      });
+    }
+  }
 }
 </script>
 
@@ -61,5 +101,10 @@ export default {
 .btn:hover {
   background: #08256d;
   cursor: pointer;
+}
+#result-container .databases {
+  max-width: 600px;
+  text-align: left;
+  margin: auto;
 }
 </style>
