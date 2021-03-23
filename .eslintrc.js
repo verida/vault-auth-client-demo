e9f8e2872142b41348c84edb085253ccad076403
Chris <chris@verida.io>
2021-03-23 15:04:58 +1030

    Add lint config

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    root: true,
+    env: {
+      node: true
+    },
+    'extends': [
+      'plugin:vue/essential',
+      '@vue/standard'
+    ],
+    rules: {
+      'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+      'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+  
+      'camelcase': 0
+    },
+    parserOptions: {
+      parser: 'babel-eslint'
+    },
+    globals: {
+      moment: true,
+      _: true
+    }
+  }
+  
